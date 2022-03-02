6c81a548f3a99d5e3b6d174ea6bc1f28da05637b
Benny Iko <benny.iko.it@gmail.com>
2022-03-01 20:22:24 -0500

    Changes to be committed: 	modified:   Main.java     On branch master 	Changes: 	Altered turn(), main() implementation 	Issues: 	N/A

--- Main.java
+++ Main.java
@@ -3,43 +3,48 @@ package aop;
 import java.util.Scanner;
 
 public class Main {
-
+	String player1, player2;
+	Board b;
+	Referee r;
+	
 	public static void main(String[] args) {
-		String player1, player2;
-		Board b = new Board();
-		Scanner in = new Scanner(System.in);
-		Referee r = new Referee(b);	
+		Main m = new Main();
+		m.b = new Board();
+		m.r = new Referee(m.b);
+		Scanner in = new Scanner(System.in);		
 		System.out.println("Welcome to AOP Tic-Tac-Toe. Created By: Benny Iko");
 		System.out.println("Player-1 enter your name:");
-		player1 = in.nextLine();
-		System.out.println("Player-1 " + player1+ " enter your symbol [X or O]:");
-		player1 = in.nextLine() + 1 + player1;
-		char p1Symbol = player1.charAt(0);
+		m.player1 = in.nextLine();
+		System.out.println("Player-1 " + m.player1+ " enter your symbol [X or O]:");
+		m.player1 = in.nextLine().toUpperCase() + 1 + m.player1;
+		char p1Symbol = m.player1.charAt(0);
 		if(p1Symbol == ('X'))
 		{
-			player2 = "O" + 2 ;
+			m.player2 = "O" + 2 ;
 		}
 		else
 		{
-			player2 = "X" + 2 ;
+			m.player2 = "X" + 2 ;
 		}
-			
 		System.out.println("Player-2 enter your name:");
-		player2 = player2 + in.nextLine();
-		String turn = player1;
+		m.player2 = m.player2 + in.nextLine();
+		String turn = m.player1;
 		
-		while(r.gameOver == false){
-			turn(player1,b);
-			b.displayBoard();
+		while(m.r.gameOver == false){
+			m.turn(m.player1);
+			m.b.displayBoard();
 			System.out.println();
 			System.out.println();
-			turn = player2;
-			turn(player2, b);
+			if(m.r.gameOver) {
+				break;
+			}		
+			turn = m.player2;
+			m.turn(m.player2);
 		}
-
+		
+		
 
 }
-	
 	
 	public static void displayOptions(){
 		System.out.print("1"); 
@@ -64,19 +69,30 @@ public class Main {
 		System.out.println();
 	}
 	
-	public static void turn(String player, Board b) {
-		System.out.println("Player" + player.substring(1, 1) + " " + player.substring(2) + " -> Your time togo. Pick the number for your spot, then press ENTER ->");
+	public Referee turn(String player) {
+		
+		System.out.println();
 		displayOptions();
 		System.out.println();
 		b.displayBoard();
+		System.out.println();
+		System.out.println("Player " + "[" + player.substring(0, 1) + "]" +  ": " + player.substring(2) + " -> Your time togo. Pick the number for your spot, then press ENTER ->");
 		var in = new Scanner(System.in);
 		var input = in.nextLine();
-		input = input.toUpperCase();
+		boolean status = b.isTaken(input);
+		
+		while(status) {
+			System.out.println("Spot Taken. Please Try Again.");
+			System.out.println("Player " + "[" + player.substring(0, 1) + "]" +  ": " + player.substring(2) + " -> Your time togo. Pick the number for your spot, then press ENTER ->");
+			input = in.nextLine();		
+			status = b.isTaken(input);
+		}
+		//System.out.println(status);
+		//input = input.toUpperCase();
 		Character c = player.charAt(0);
 		var symbol = c.toString();	
 		
 		switch(input) {
-		
 		case "1": 
 			b.changeTopLeft(symbol);
 			break;
@@ -103,9 +119,9 @@ public class Main {
 			break;
 		case "9":
 			b.changeBottomRight(symbol);
-			break;
-				
-		
+			break;		
 		}
+		
+		return r;
 	}
 }
